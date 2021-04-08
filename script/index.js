5eb1aaaeb67db922affe2f0fff7152dc65d70588
Nikolay Salnikov <nikolay.salnikov@gmail.com>
2021-04-09 02:38:33 +0300

    JS optimisation + Html/css bugfix

--- script/index.js
+++ script/index.js
@@ -3,33 +3,35 @@ let popup = document.querySelector('.popup');
 let close = document.querySelector('.popup__button');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubTitle = document.querySelector('.profile__subtitle');
-let formElement = document.querySelector('.edit-profile');
+let formElement = document.querySelector('.form');
 let name = document.querySelector('#name');
 let job = document.querySelector('#job');
 let formButton = document.querySelector('.form__button');
-let formValueName = document.querySelector('.form__item_name');
-let formValueJob = document.querySelector('.form__item_job');
 let popupOpened = document.querySelector('.popup_opened')
 
-edit.addEventListener('click', () => {
-    formValueName.value = profileTitle.textContent;
-    formValueJob.value = profileSubTitle.textContent;
-});
 
-edit.onclick = function popupOpen() {
+/* Функция на открытие popup */
+function popupOpen() {
     popup.classList.add('popup_opened');
 }
 
-/* close.onclick = function popupClose() {
-    popup.classList.remove('popup_opened');
-} */
-
+/* Функция на закрытие popup */
 function popupClose() {
     popup.classList.remove('popup_opened');
 }
 
+/* Открываю окно по клику на edit + заполняю value */
+edit.addEventListener('click', () => {
+    popupOpen();
+    name.value = profileTitle.textContent;
+    job.value = profileSubTitle.textContent;
+
+});
+
+/* При клике на крестик закрываю форму без изменений */
 close.addEventListener('click', popupClose);
 
+/* При сохранении формы меняются значения в html и вызывается функция на закрытие формы */
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileTitle.textContent = name.value;
@@ -38,5 +40,5 @@ function formSubmitHandler(evt) {
 }
 
 
-
+/* Выполнянем функцию при нажатии на Сохранить */
 formElement.addEventListener('submit', formSubmitHandler);
